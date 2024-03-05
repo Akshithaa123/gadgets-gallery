1afdd12a262526fc192118be6601e5db01a93c15
Akshithaa123 <144244351+Akshithaa123@users.noreply.github.com>
2024-03-05 12:22:45 +0530

    Add files via upload

---
+++ gadgets-product-list.js
@@ -0,0 +1,65 @@
+var products = [{
+    index: 1,
+    id: 'p1',
+    name: 'Apple Airpods',
+    price: 50000,
+    link: 'product1.jepg'
+},
+{
+    index: 2,
+    id: 'p2',
+    name: 'Ipad Gen 8',
+    price: 58000,
+    link: 'product2.jpeg'
+},
+{
+    index: 3,
+    id: 'p3',
+    name: 'Iphone 13',
+    price: 89000,
+    link: 'product3.jpeg'
+},
+{
+    index: 4,
+    id: 'p4',
+    name: 'Iphone 14',
+    price: 100000,
+    link: 'product4.jpeg'
+},
+{
+    index: 5,
+    id: 'p5',
+    name: 'Apple Watch',
+    price: 95000,
+    link: 'product5.jpeg'
+},
+{
+    index: 6,
+    id: 'p6',
+    name: 'Mac Book',
+    price: 175000,
+    link: 'product6.jpeg'
+},
+{
+    index: 7,
+    id: 'p7',
+    name: 'Iphone 14 Pro Max',
+    price: 150000,
+    link: 'product7.jpeg'
+},
+{
+    index: 8,
+    id: 'p8',
+    name: 'Mac Book',
+    price: 250000,
+    link: 'product8.jpeg'
+},
+{
+    index: 9,
+    id: 'p9',
+    name: 'I watch',
+    price: 300000,
+    link: 'product9.jpeg'
+},
+
+]
